61a4031d085568e57b49700c85a385c9e1bce17b
Stefan Huber <sh@signalwerk.ch>
2019-10-19 00:13:23 +0200

    ADD: m0ire.com

--- domains/moire.js
+++ domains/moire.js
@@ -2,5 +2,16 @@ D(
   "moire.pub",
   REG_NONE,
   DnsProvider(CLOUDFLARE),
-  A("@", "87.230.27.223", TTL(1))
+  A("@", "87.230.27.223", TTL(1)),
+  CNAME("*", "moire.pub.", TTL(1)),
+  CNAME("www", "moire.pub.", TTL(1))
+);
+
+D(
+  "m0ire.com",
+  REG_NONE,
+  DnsProvider(CLOUDFLARE),
+  A("@", "87.230.27.223", TTL(1)),
+  CNAME("*", "m0ire.com.", TTL(1)),
+  CNAME("www", "m0ire.com.", TTL(1))
 );
