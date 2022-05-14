96e686012e3bfb97876006084472ebadc2bc2685
Stefan Huber <sh@signalwerk.ch>
2022-05-15 00:53:59 +0200

    Update caminantes-grafico.js

--- domains/caminantes-grafico.js
+++ domains/caminantes-grafico.js
@@ -2,8 +2,6 @@ D(
   "caminantes-grafico.org",
   REG_NONE,
   DnsProvider(CLOUDFLARE),
-
-  // root & www
   GITHUB_A,
-  CNAME("www", "@", TTL(1), { cloudflare_proxy: "on" }),
+  CNAME("www", GITHUB_SIGNALWERK_CNAME, TTL(1))
 );
