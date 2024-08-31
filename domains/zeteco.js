76f82b9db5a6f4c727f839cbd5534ad389aa3d86
Stefan Huber <sh@signalwerk.ch>
2024-08-31 16:21:24 +0200

    Update for fahrplan.zeteco.ch

--- domains/zeteco.js
+++ domains/zeteco.js
@@ -6,5 +6,9 @@ D(
   // root & www
   GITHUB_A_PROXY,
   CNAME("www", "@", TTL(1), { cloudflare_proxy: "on" }),
-  CNAME("wiki", "@", TTL(1), { cloudflare_proxy: "on" })
+  CNAME("wiki", "@", TTL(1), { cloudflare_proxy: "on" }),
+
+  // according to https://md.coredump.ch/protokoll-gettogether-20240830
+  A("fahrplan", "77.109.139.213", TTL(1)),
+  AAAA("fahrplan", "2001:1620:2047::213", TTL(1))
 );
